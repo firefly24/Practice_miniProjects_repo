3abcba3be40f18b1bbb4f9194c7a88fec1e9b61d
firefly24 <hotkar.darshana@gmail.com>
2025-09-24 16:26:41 +0530

    refine APIs for spsc and add support for move only objects to be part of queue (classes where copy constructor is deleted)

--- cpp_practice_mini_projects/simple_lock_free_queue/lock_free_queue.h
+++ cpp_practice_mini_projects/simple_lock_free_queue/lock_free_queue.h
@@ -41,7 +41,7 @@ template <typename T>
 class lockFree_spsc_Queue
 {
 private:
-    size_t capacity;
+    size_t capacity_;
     std::vector<T> ring_buff;
     std::atomic<size_t> head; // Index of first item
     std::atomic<size_t> tail; // Index of the next empty slot after last item
@@ -50,15 +50,15 @@ private:
     // This function helps to increment the head/tail index such that it wraps around the ring buffer
     size_t advance(size_t index) const
     {
-        return (index+1)%capacity; // while advancing index, wrap around the ring buffer
+        return (index+1)%capacity_; // while advancing index, wrap around the ring buffer
     }
 
 public:
 
-    explicit lockFree_spsc_Queue (size_t Capacity): head(0), tail(0)
+    explicit lockFree_spsc_Queue (size_t Capacity): capacity_(Capacity+1), head(0), tail(0)
     {
-        capacity = Capacity;
-        ring_buff = std::vector<T>(capacity+1); // keep one empty element between head and tail to check if buffer full/empty
+        //capacity = Capacity;
+        ring_buff = std::vector<T>(capacity_); // keep one empty element between head and tail to check if buffer full/empty
     }
 
     lockFree_spsc_Queue(const lockFree_spsc_Queue&) = delete;
@@ -68,7 +68,7 @@ public:
 
     // Push item by Producer to the end of the queue
     //copy version
-    void push(const T& item)
+    bool push(const T& item)
     {
         // Only producer thread exclusively modifies the tail of the buffer, whereas consumer thread has no reason to modify tail.
         // So we can load tail idx in relaxed order in producer thread
@@ -81,18 +81,47 @@ public:
         // So we will use acquire ordering for head to ensure getting an up-to-date head 
         if ((next = advance(back)) == head.load(std::memory_order_acquire))
         {
+#ifdef DEBUG
             std::cout << "Queue is full" << std::endl;
-            return;
+#endif
+            return false;
         }
         ring_buff[back] = item;
         // Producer(this) thread need to publish the incremented tail, only after the item is inserted to the queue,
         // so ordering is important here, and so we use release ordering to publish new tail only after above op is done
         tail.store(next,std::memory_order_release);  
+        return true;
+    }
+
+    // Push for move version
+    bool push(T&& item)
+    {
+        // Only producer thread exclusively modifies the tail of the buffer, whereas consumer thread has no reason to modify tail.
+        // So we can load tail idx in relaxed order in producer thread
+
+        size_t back = tail.load(std::memory_order_relaxed);
+        size_t next;
+
+        // Check if queue is full
+        // Since head can be modified by consumer thread, we would want the consumer to finish updating the head before publishing it to producer(this) 
+        // So we will use acquire ordering for head to ensure getting an up-to-date head 
+        if ((next = advance(back)) == head.load(std::memory_order_acquire))
+        {
+#ifdef DEBUG
+            std::cout << "Queue is full" << std::endl;
+#endif
+            return false;
+        }
+        ring_buff[back] = std::move(item);
+        // Producer(this) thread need to publish the incremented tail, only after the item is inserted to the queue,
+        // so ordering is important here, and so we use release ordering to publish new tail only after above op is done
+        tail.store(next,std::memory_order_release);  
+        return true;
     }
 
     // Remove/consume item by consumer thread from the front of the queue
     //TODO : fix return value on empty queue
-    T pop()
+    bool pop(T &out)
     {
         // Consumer thread will only consume from the head of the queue, so it will advance only head idx and not modify tail idx
         size_t front = head.load(std::memory_order_relaxed);
@@ -102,15 +131,30 @@ public:
         // so we need to impose an order here as well, that consumer views the updated tail published by consumer
         if (front == tail.load(std::memory_order_acquire))
         {
+#ifdef DEBUG
             std::cout << "Queue is empty" << std::endl;
-            return NULL;
+#endif
+            return false;
         }
-        T ret = ring_buff[front];
+        // works for both copyable and non-copyable, as direct assignment failed for when T = unqiue_ptr 
+        out = std::move(ring_buff[front]);
+        //out = ring_buff[front];
         head.store(advance(front), std::memory_order_release);
         // published the updated head after consuming front element
-        return ret;
+        return true;
     }
 
+    size_t capacity() const
+    {
+        return (capacity_ -1);
+    }
+
+    size_t size() const
+    {
+        return((capacity_+tail.load(std::memory_order_relaxed)) - head.load(std::memory_order_relaxed) )%capacity_;
+    }
+
+    /*
     bool empty() const {
         return (head.load(std::memory_order_acquire) == tail.load(std::memory_order_acquire));
     }
@@ -119,4 +163,5 @@ public:
         size_t next = advance(tail.load(std::memory_order_acquire));
         return next == head.load(std::memory_order_acquire);
     }
+    */
 };
