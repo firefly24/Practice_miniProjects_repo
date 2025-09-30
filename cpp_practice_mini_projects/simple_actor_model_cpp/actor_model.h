5e9313646081fd90b24b5ca9e85b120973ab6041
firefly24 <hotkar.darshana@gmail.com>
2025-09-30 23:10:23 +0530

    add more parameters and refine looping logic

--- cpp_practice_mini_projects/simple_actor_model_cpp/actor_model.h
+++ cpp_practice_mini_projects/simple_actor_model_cpp/actor_model.h
@@ -1,76 +1,142 @@
 #include <cstddef>
 #include <thread>
 #include <functional>
+#include <semaphore>
+#include <string>
+#include <memory>
 #include "../simple_mpmc_queue/mpmc_queue_bounded.h"
 
 template <typename Task> class Actor;
-template <typename Task> class ActorModel;
-
-template <typename T>
-void send_msg(Actor<T>&,T);
+template <typename Task> class ActorSystem;
 
 template <typename Task>
 class Actor
 {
-
 private:
-    size_t id_;
-    mpmcQueueBounded<Task> mailbox_q;
+    std::unique_ptr<mpmcQueueBounded<Task>> mailbox_q;
     std::atomic<bool> actor_alive_;
     std::thread dispatcher_;
+    // TODO how to use counting semaphore here, while using 
+    //std::binary_semaphore new_mail_arrived_{0};
+    std::counting_semaphore<> new_mail_arrived_{0};
 
+    // Open a msg from mailbox and execute the task
     void receive()
     {
-        //std::cout << "receive" << std::endl;
         std::function<void()> newTask;
-        if(mailbox_q.try_pop(newTask)) 
+        if(mailbox_q->try_pop(newTask))
+        {
+#ifdef DEBUG 
+            std::cout << name_ << ": "; 
+#endif
             newTask();
-        
+        }
     }
-
+    // Keep polling mailbox for new tasks
     void checkMailbox()
     {
+        // TODO : if actor_alive_ is false, but the mailbox still has some tasks pending, 
+        //need to add mechanims to complete those tasks
         while(actor_alive_.load(std::memory_order_relaxed))
         {
-            
-            if (!mailbox_q.empty())
-                receive();
+            new_mail_arrived_.acquire();
+            receive();
         }
-        //std::cout << "Stopping dispatcher thread" <<std::endl;
+        std::cout <<name_ <<": Stopping dispatcher thread" <<std::endl;
     }
 
 public:
-    Actor(size_t mailbox_size): mailbox_q(mailbox_size), actor_alive_(true)
+    size_t id_;
+    std::string name_;
+
+    Actor(size_t mailbox_size, size_t id):actor_alive_(true),id_(id)
     {
+        name_ ="";
+        mailbox_q = std::make_unique<mpmcQueueBounded<Task>>(mailbox_size);
+        dispatcher_ = std::thread([this](){checkMailbox();});
+
+    }
+
+    Actor(size_t mailbox_size, size_t id,std::string name):actor_alive_(true),id_(id),name_(name)
+    {
+        mailbox_q = std::make_unique<mpmcQueueBounded<Task>>(mailbox_size);
         dispatcher_ = std::thread([this](){checkMailbox();});
     }
 
-    friend void send_msg<Task>(Actor<Task>&,Task);
+    // Push a task to this actor's mailbox
+    void addToMailbox(Task task)
+    {
+        if (actor_alive_.load(std::memory_order_relaxed))
+        {
+            while (!mailbox_q->try_push(task)){
+                // TODO: add some terminate logic here to prevent spinning forever
+            }   
+            new_mail_arrived_.release();
+        }
+    }
 
+    // Stop the mailbox checker thread
     void stopActor()
     {
-        actor_alive_.store(false);
+        actor_alive_.store(false,std::memory_order_release);
+        new_mail_arrived_.release();
         if(dispatcher_.joinable())
             dispatcher_.join();
     }
 
     ~Actor()
     {
-        //std::cout << "Desctrutor Called" << std::endl;
+        std::cout << name_ << ": Destrutor Called" << std::endl;
         if (actor_alive_)
             stopActor();
     }
+
+    //delete copy constructors
+    Actor(const Actor&) = delete;
+    Actor operator=(const Actor&) = delete;
+
+    //set move constuctors to default;
+    Actor(Actor&&) = default;
+    Actor& operator=(Actor &&) = default;
+
 };
 
-template <typename Task>
-void send_msg(Actor<Task>& actor,Task task)
-{
-    while (!actor.mailbox_q.try_push(task)){}   
-}
-
+// Function that constucts the task from given parameters and adds it to the actor's mailbox
 template <typename Task, typename Func, typename... Args>
 void send(Actor<Task>& actor,Func&& func, Args&&... args)
 {
     Task task = std::bind(std::forward<Func>(func), std::forward<Args>(args)...);
-    send_msg(actor,task);
+    actor.addToMailbox(task);
 }
+
+template <typename Task>
+class ActorSystem
+{
+private:
+    std::atomic<size_t> active_actors_;
+    size_t total_actors_;
+
+public:
+
+    std::vector<std::shared_ptr<Actor<Task>>> actor_pool_;
+    ActorSystem(size_t numActors):   active_actors_(0), total_actors_(numActors) {
+        actor_pool_=std::vector<std::shared_ptr<Actor<Task>>>(numActors,nullptr) ;
+    }
+
+    std::shared_ptr<Actor<Task>> spawn(size_t mailbox_capacity=1,std::string name = "")
+    {
+        size_t curr_size;
+        while(1)
+        {
+            curr_size = active_actors_.load(std::memory_order_relaxed);
+            if (curr_size >= total_actors_)
+                return nullptr;
+            if (active_actors_.compare_exchange_weak(curr_size,curr_size+1, std::memory_order_release,std::memory_order_acquire))
+            {
+                actor_pool_[curr_size] = std::make_shared<Actor<Task>>(mailbox_capacity,curr_size,name);
+                return actor_pool_[curr_size];
+            }
+            // TODO: Do I need backoff strategy here ? 
+        }
+    }
+};
--- cpp_practice_mini_projects/simple_actor_model_cpp/actor_model_test.cpp
+++ cpp_practice_mini_projects/simple_actor_model_cpp/actor_model_test.cpp
@@ -5,25 +5,53 @@
 
 using Job = std::function<void()> ;
 
+void pingpong(Actor<Job>& sender, Actor<Job>& receiver)
+{
+    std::cout << "Ping" << std::endl;
+
+    auto getack = [](){std::cout << "Pong" << std::endl; };
+
+    send(sender,getack);
+}
+
+
+void testPingpong()
+{
+    Actor<Job> dummy{5,1,"dummy1"};
+    Actor<Job> dummy2{5,2,"dummy2"};
+
+    send(dummy2,pingpong,std::ref(dummy),std::ref(dummy2));
+    std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    return;
+}
+
+void testActorSystem()
+{
+    ActorSystem<Job> ActorAdmin(1);
+    int total_msgs=20;
+    std::function<void(int)> myFunc = [](int a){ std::cout << "This is my task:" << a << std::endl; };
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    std::shared_ptr<Actor<Job>> asActor = ActorAdmin.spawn(10,"Actor1");
+
+    for (int i=1;i<=total_msgs;i++)
+    {
+        send(*asActor,myFunc,i);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto time_taken = std::chrono::duration_cast<std::chrono::milliseconds>(end-start).count();
+    double throughput = ((double)total_msgs) / (time_taken / 1000.0);
+
+    std::cout << "Elements:" << total_msgs << "\nTime taken: " << time_taken << "ms\nThroughput: " << throughput << std::endl;
+}
 
 int main()
 {
-    Actor<Job> dummy{10};
-
-    std::function<void(int)> myFunc = [](int a){ std::cout << "This is my task" << a << std::endl; };
-
-    for (int i=1;i<=10;i++)
-    {
-        send(dummy,myFunc,i);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
-    dummy.stopActor();
-
-    //for (int i=1;i<=10;i++)
-    {
-        send(dummy,myFunc,11);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
-
+    testPingpong();
+    testActorSystem();
     return 0;
 }
