823dee3051603991575f2bb7b74840268313e372
firefly24 <hotkar.darshana@gmail.com>
2026-01-26 21:58:27 +0530

    since this is purely sw watchdog timer, ditch the platform device approach and use a simple kernel module instead

--- Drivers_pract/drivers/simple_wd_timer/src/sw_wd_timer_mod.c
+++ Drivers_pract/drivers/simple_wd_timer/src/sw_wd_timer_mod.c
@@ -3,8 +3,8 @@
 #include <linux/init.h>
 #include <linux/err.h>
 #include <linux/jiffies.h>
-#include <linux/platform_device.h>
-#include <linux/device.h>
+//#include <linux/platform_device.h>
+//#include <linux/device.h>
 #include <linux/timer.h>
 
 MODULE_LICENSE("GPL");
@@ -15,49 +15,58 @@ MODULE_DESCRIPTION("Simple watchdog timer practice");
 
 struct sw_wd_timer{
 
-	struct device *dev;
+	//struct device *dev;
 
 	struct timer_list timer_work;
 	
 	unsigned long timeout_jiffies;
 	unsigned long last_pet_jiffies;
 
-	atomic_t fired_count;
-	atomic_t enabled;
+	atomic_t ping_count;
+	atomic_t is_active;
 
-	spinlock_t slock;
+	//spinlock_t slock;
 };
 
-static void sw_wd_timer_ping(struct sw_wd_timer *wd)
+// writing the module for one instance only for now
+static struct sw_wd_timer wd;
+
+static void sw_wd_timer_ping(struct sw_wd_timer *swd)
 {
-	if (IS_ERR_OR_NULL(wd))
+	if (IS_ERR_OR_NULL(swd))
 		return;
 	
-	if(!atomic_read(&wd->enabled))
+	if(!atomic_read(&swd->is_active))
 		return;	
+		
+	atomic_add(1,&swd->ping_count);
+	swd->last_pet_jiffies = jiffies;
 	
-	mod_timer(&wd->timer_work,jiffies +  wd->timeout_jiffies);
+	mod_timer(&swd->timer_work,jiffies +  swd->timeout_jiffies);
 }
 
-static void sw_wd_timer_start(struct sw_wd_timer *wd)
+static void sw_wd_timer_start(struct sw_wd_timer *swd)
 {
-	if(IS_ERR_OR_NULL(wd))
+	if(IS_ERR_OR_NULL(swd))
 		return;
 	
-	atomic_set(&wd->enabled,1);
+	atomic_set(&swd->ping_count,0);
+	swd->last_pet_jiffies = jiffies;
 	
-	mod_timer(&wd->timer_work, jiffies + wd->timeout_jiffies);	
+	atomic_set(&swd->is_active,1);
 	
-	pr_info("%s:\t Watchdog timer enabled, timeout: %lu\n",__func__, wd->timeout_jiffies);
+	mod_timer(&swd->timer_work, jiffies + swd->timeout_jiffies);	
+	
+	pr_info("%s:\t Watchdog timer enabled, timeout: %lu\n",__func__, swd->timeout_jiffies);
 }
 
-static void sw_wd_timer_stop(struct sw_wd_timer *wd)
+static void sw_wd_timer_stop(struct sw_wd_timer *swd)
 {
-	atomic_set(&wd->enabled,0);
+	atomic_set(&swd->is_active,0);
 	
-	timer_delete_sync(&wd->timer_work);
+	timer_delete_sync(&swd->timer_work);
 	
-	pr_info("%s:\t Watchdog timer disabled, timeout: %lu\n",__func__, wd->timeout_jiffies);
+	pr_info("%s:\t Watchdog timer disabled, timeout: %lu\n",__func__, swd->timeout_jiffies);
 }
 
 
@@ -65,57 +74,54 @@ static void sw_wd_timer_work_func(struct timer_list *t)
 {
 	struct sw_wd_timer *swd = container_of(t,struct sw_wd_timer,timer_work);
 	
-	pr_info("%s:\t SW Watchdog interrupt fired\n",__func__);  
+	pr_info("%s:\t SW Watchdog interrupt fired, last pet time: %lu, time elapsed: %lu\n",
+		__func__,
+		swd->last_pet_jiffies,
+		jiffies_to_msecs(jiffies - swd->last_pet_jiffies));  
+	
+	if (!atomic_read(&swd->is_active))
+		return;
+	
+	panic("SW watchdog expired!");
 }
 
-static int sw_wd_timer_probe(struct platform_device *pdev)
+static int __init sw_wd_timer_init(void)
 {
-	struct sw_wd_timer *wd = devm_kzalloc(&pdev->dev,sizeof(*wd),GFP_KERNEL);
+	//struct sw_wd_timer *wd = devm_kzalloc(&pdev->dev,sizeof(*wd),GFP_KERNEL);
 
-	if(!wd)
-		return -ENOMEM;
+	//if(!wd)
+	//	return -ENOMEM;
 
-	wd->dev = &pdev->dev;
+	//wd->dev = &pdev->dev;
 
-	atomic_set(&wd->fired_count,0);
+	wd.timeout_jiffies = msecs_to_jiffies(WD_DEFAULT_TIMEOUT_MS);
 
-	wd->timeout_jiffies = msecs_to_jiffies(WD_DEFAULT_TIMEOUT_MS);
+	timer_setup(&wd.timer_work, sw_wd_timer_work_func,0);
 
-	wd->last_pet_jiffies = jiffies;
+	//platform_set_drvdata(pdev,wd);
 
-	timer_setup(&wd->timer_work, sw_wd_timer_work_func,0);
-
-	platform_set_drvdata(pdev,wd);
-
-	dev_info(&pdev->dev,"%s:\t Software watchdog module loaded succesfully\n",__func__);
+	pr_info("%s:\t Software watchdog module loaded succesfully\n",__func__);
 	
-	sw_wd_timer_start(wd);
+	sw_wd_timer_start(&wd);
 
 	return 0;
 }
 
-static void sw_wd_timer_remove(struct platform_device *pdev)
+static void __exit sw_wd_timer_exit(void)
 {
-	struct sw_wd_timer *wd = platform_get_drvdata(pdev);
+	//struct sw_wd_timer *wd = platform_get_drvdata(pdev);
 	
-	sw_wd_timer_stop(wd);
+	sw_wd_timer_stop(&wd);
 
-	timer_shutdown_sync(&wd->timer_work);
+	timer_shutdown_sync(&wd.timer_work);
 
-	dev_info(&pdev->dev,"%s:\t Successfully removed sw watchdog module\n",__func__);
+	pr_info("%s:\t Successfully removed sw watchdog module\n",__func__);
 }
 
+module_init(sw_wd_timer_init);
+module_exit(sw_wd_timer_exit);
 
-static struct platform_driver sw_wd_timer_driver = {
-	.probe = sw_wd_timer_probe,
-	.remove = sw_wd_timer_remove,
-	.driver = {
-		.name = "sw_wd_timer_mod",
-	},
-
-};
-
-module_platform_driver(sw_wd_timer_driver);
+//module_platform_driver(sw_wd_timer_driver);
 
 
 
