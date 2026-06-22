2148e127a22963536e29987f79ace9ca1a4ea915
firefly24 <hotkar.darshana@gmail.com>
2026-06-23 00:33:07 +0530

    V1: Add and test a basic producer thread pushing telemetry records to ring buffer

--- Drivers_pract/drivers/telemetry_simple/src/telemetry_main.c
+++ Drivers_pract/drivers/telemetry_simple/src/telemetry_main.c
@@ -5,11 +5,17 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/slab.h>
+#include <linux/ktime.h>
+#include <linux/delay.h>
+#include <linux/sched.h>
+#include <linux/kthread.h>
 #include "telemetry_ring.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("firelfy24");
 MODULE_DESCRIPTION("Simple telemetry module for practice");
+
+#define PRODUCER_SLEEP_MS 1000
 
 struct telemetry_dev{
 	
@@ -38,51 +44,56 @@ struct telemetry_dev{
 
 struct telemetry_dev *tdev;
 
-void test_push_pop(void)
+static int producer_thread_fn(void *data)
 {
-	int capacity,i=0;
+	struct telemetry_dev *tdev = data;
 	struct telemetry_record *record;
+	uint64_t seq_no =0;
+	
+	if(data == NULL)
+		return -EINVAL;
+		
+	if (tdev->buf.records == NULL)
+		return -EINVAL;
 	
 	record = kzalloc(sizeof(struct telemetry_record), GFP_KERNEL);
 	
-	if(!record)
-		return ;//-ENOMEM;
+	if (!record)
+		return -ENOMEM;
+	
+	while(!kthread_should_stop())
+	{
+		// generate record
+		record->seq_no = seq_no++;
+		record->timestamp_ms = ktime_to_ms(ktime_get());
+		record->value = 42;
 		
-	capacity= tdev->buf.capacity;
-	
-	for (i=0;i<=capacity;i++)
-	{
-		record->value = i *10;
-		record->seq_no = i;
-		record->timestamp_ms = 0;
-		if(ring_push(&tdev->buf,record))
+		// push to ring buffer
+		//ring_push(&tdev->buf, record);
+		if ( ring_push(&tdev->buf,record) == 0 )
 		{
-			printk(KERN_INFO "Fail insert %d\n",i);
-		}
-	}
-	
-	for(i=0;i<=capacity;i++)
-	{
-		if ( ring_pop(&tdev->buf,record) == 0 )
-		{
-			printk(KERN_INFO "Successfully popped: %d\n",record->value );
+			printk(KERN_INFO "Successfully pushed: %llu\n",record->seq_no );
 		}
 		else
 		{
-			printk(KERN_INFO "Failed to pop%d\n",i);
+			printk(KERN_INFO "Failed to push%llu\n",record->seq_no);
 		}
-	
+		
+		//sleep 
+		msleep(PRODUCER_SLEEP_MS);
 	}
+	
+	printk(KERN_INFO "Stopping producer thread\n");
 	
 	kfree(record);
 	
-	return;
+	return 0;
 }
 
 static int __init telemetry_dev_init(void)
 {
 	int ret=0;
-	uint32_t capacity = 4;
+	uint32_t capacity = 5;
 	
 	tdev = kzalloc(sizeof(struct telemetry_dev),GFP_KERNEL);
 	if (!tdev)
@@ -90,17 +101,48 @@ static int __init telemetry_dev_init(void)
 	
 	printk(KERN_INFO "Telemetry_init\n");
 		
+	/* Create and initialize ring buffer for telemetry records */
 	if ((ret = ring_init(&tdev->buf, capacity)) )
-		return  ret;
+		goto free_dev;
+		
+	/* Create and initialize kthread */
+	tdev->producer_thread = kthread_create(producer_thread_fn,(void *) tdev,"Producer_telemetry");
 	
-	test_push_pop();
+	if(IS_ERR(tdev->producer_thread))
+	{
+		ret = PTR_ERR(tdev->producer_thread);
+		goto free_ring_buf;
+	}
+	
+	/* run the producer thread */
+	wake_up_process(tdev->producer_thread);
+	
+	// Wait for some time to let producer thread run
+	msleep(10000);
+	
+	// stop producer thread
+	kthread_stop(tdev->producer_thread);
+	
+	// set producer thread to NULL to avoid double kthread_stop
+	tdev->producer_thread = NULL;
 	
 	return 0;
+	
+	
+free_ring_buf:	
+	ring_destroy(&tdev->buf);
+free_dev:
+	kfree(tdev);
+	
+	return ret;
 }
 
 static void __exit telemetry_dev_exit(void)
 {
-
+	if (!tdev)
+		return;
+	if (tdev->producer_thread != NULL)	
+		kthread_stop(tdev->producer_thread);
 	ring_destroy(&tdev->buf);
 	kfree(tdev);
 	
